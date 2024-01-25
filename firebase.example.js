506056fa111310ce3c56958923343afa17c7fef0
Josemar Cafumana <rjosemar-cafumana@hotmail.com>
2024-01-25 22:27:52 +0100

    feat: firebase config example

---
+++ firebase.example.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
+
+export { firebase, db }
+
